9d01d92135f4ec125b005562d46dd97abd4e57ae
carolinaalfaro <37843530+carolinaalfaro@users.noreply.github.com>
2019-05-10 15:14:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+var stick = header.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset > sticky) 
+window.onscroll = function() {myFunction()};
